325441cb183e4c777dd87f36dbe9d50350e8689f
MaryleneH <marylene.henry@gmail.com>
2025-07-22 13:15:48 +0000

    essai modèle Mistral

--- smolagents/minimal_example.py
+++ smolagents/minimal_example.py
@@ -4,7 +4,8 @@ import os
 mon_token = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
 # Initialize a model (using Hugging Face Inference API)
-model = InferenceClientModel(token=mon_token)  # Uses a default model
+model = InferenceClientModel(token=mon_token,
+                             model_id="mistralai/Mistral-7B-Instruct-v0.3")
 
 # Create an agent with no tools
 agent = CodeAgent(tools=[], model=model)
@@ -16,11 +17,12 @@ print(result)
 print(result2)
 
 
-### Adding Tools
+# Adding Tools
 
 from smolagents import CodeAgent, InferenceClientModel, DuckDuckGoSearchTool
 
-model = InferenceClientModel(token=mon_token)
+model = InferenceClientModel(token=mon_token,
+                             model_id="mistralai/Mistral-7B-Instruct-v0.3")
 agent = CodeAgent(
     tools=[DuckDuckGoSearchTool()],
     model=model,
